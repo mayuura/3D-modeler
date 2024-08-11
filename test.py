46ea97e4881ee267913c2e6bd8d757e682612b2f
mayuura <mehdi61049@gmail.com>
2024-08-11 21:06:58 +0200

    formatting fixed

--- test.py
+++ test.py
@@ -5,17 +5,25 @@ Created on Thu Aug  8 21:40:26 2024
 @author: El Mehdi
 """
 
-from OpenGL.GL import glClearColor, glColor3f, glPointSize, glClear, glBegin, glVertex2f, glEnd, glFlush
-from OpenGL.GL import GL_COLOR_BUFFER_BIT, GL_POINTS
-from OpenGL.GLUT import glutInit, glutInitDisplayMode, glutInitWindowSize, glutInitWindowPosition, glutCreateWindow, glutDisplayFunc, glutMainLoop
-from OpenGL.GLUT import GLUT_SINGLE, GLUT_RGB
+from OpenGL.GL import (
+    glClearColor, glColor3f, glPointSize, glClear, 
+    glBegin, glVertex2f, glEnd, glFlush, GL_COLOR_BUFFER_BIT, 
+    GL_POINTS
+)
+from OpenGL.GLUT import (
+    glutInit, glutInitDisplayMode, glutInitWindowSize, 
+    glutInitWindowPosition, glutCreateWindow, glutDisplayFunc, 
+    glutMainLoop, GLUT_SINGLE, GLUT_RGB
+)
 from OpenGL.GLU import gluOrtho2D
 
+
 def myInit():
-    glClearColor(1.0, 1.0, 0.0, 1.0)
+    glClearColor(0, 0, 1.0, 1.0)
     glColor3f(0.2, 0.5, 0.4)
     glPointSize(10.0)
     gluOrtho2D(0, 500, 0, 500)
+
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT)
@@ -27,11 +35,12 @@ def display():
 
     glFlush()
 
+
 glutInit()
-glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)   
-glutInitWindowSize(500, 500)  
-glutInitWindowPosition(100, 100)  
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+glutInitWindowSize(500, 500)
+glutInitWindowPosition(100, 100)
 glutCreateWindow("Test window")
 myInit()
-glutDisplayFunc(display) 
+glutDisplayFunc(display)
 glutMainLoop()
